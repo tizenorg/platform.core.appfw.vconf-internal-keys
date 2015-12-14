fc5c50bb24bf73ba6e623dde87ab83c7c2e122e2
MyoungJune Park <mj2004.park@samsung.com>
2015-12-14 14:06:37 +0900

    add VCONFKEY_IDLE_LOCK_STATE_READ_ONLY for lock state
    Change-Id: I0571132d66b502f7236e1db2d0782a50572298ec
    Signed-off-by: MyoungJune Park <mj2004.park@samsung.com>

--- scripts/vconf-internal-idle-lock-keys.sh
+++ scripts/vconf-internal-idle-lock-keys.sh
@@ -3,3 +3,5 @@
 /usr/bin/buxton2ctl -i -d create-string "system" "db/idle_lock/bgset"  "/usr/apps/org.tizen.setting/shared/res/settings/Wallpapers/Home_default.jpg"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
 
 /usr/bin/buxton2ctl -i -d create-int32 "memory" "memory/idle_lock/state"  "0"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
+
+/usr/bin/buxton2ctl -i -d create-int "memory" "memory/idle_lock/state_read_only"  "0"  "http://tizen.org/privilege/internal/default/public" "http://tizen.org/privilege/internal/default/platform"
