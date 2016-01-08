fa58a4ded5aa710e209474947a1ea4243e8bdc5b
MyoungJune Park <mj2004.park@samsung.com>
2016-01-08 13:33:29 +0900

    add build feature ( mobile, wearable ) by model name (DEMO)
    Change-Id: Ie8a519584810f7277d4010d86542474fee50ad48
    Signed-off-by: MyoungJune Park <mj2004.park@samsung.com>

--- scripts/vconf-internal-browser-keys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# DO NOT GENERATE # /usr/bin/buxton2ctl -i -d create-int32 "" "db/browser"  "0"  "" ""
-
-# DO NOT GENERATE # /usr/bin/buxton2ctl -i -d create-int32 "system" "db/browser/browser_user_agent"  "0"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
-
-# DO NOT GENERATE # /usr/bin/buxton2ctl -i -d create-int32 "system" "db/browser/custom_user_agent"  "0"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
