3e14023dfc911df8e2abc062a4a41778be2daff0
MyoungJune Park <mj2004.park@samsung.com>
2016-01-08 13:33:29 +0900

    add build feature ( mobile, wearable ) by model name
    Change-Id: Ie8a519584810f7277d4010d86542474fee50ad48
    Signed-off-by: MyoungJune Park <mj2004.park@samsung.com>

--- scripts/vconf-internal-filemanager-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/buxton2ctl -i -d create-int32 "memory" "memory/filemanager/Mmc"  "0"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
-
-/usr/bin/buxton2ctl -i -d create-int32 "system" "db/filemanager/dbupdate"  "1"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
