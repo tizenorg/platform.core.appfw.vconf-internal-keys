131e69c46aa40e4a5224506bcafe884c6febb133
MyoungJune Park <mj2004.park@samsung.com>
2016-01-08 13:33:29 +0900

    Add build feature ( ex) mobile, wearable or model type)
    - DONE add XSD xml scheme check code
    - DONE test if default.sh, product.sh are working.
    - DONE make build-feature disabled until further discussion.
      - it will be able to do if discussion is ready.
    
    Change-Id: Ie8a519584810f7277d4010d86542474fee50ad48
    Signed-off-by: MyoungJune Park <mj2004.park@samsung.com>

--- scripts/vconf-internal-usb-keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/buxton2ctl -i -d create-int32 "memory" "memory/usb/cur_mode"  "0"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
-
-/usr/bin/buxton2ctl -i -d create-int32 "system" "db/usb/sel_mode"  "1"  "http://tizen.org/privilege/internal/default/platform" "http://tizen.org/privilege/internal/default/platform"
