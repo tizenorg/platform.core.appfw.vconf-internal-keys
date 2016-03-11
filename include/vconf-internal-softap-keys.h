7c2da3932ba09fb2ebc9b082f04f47c3b0b8aeb2
Seonah Moon <seonah1.moon@samsung.com>
2016-03-11 15:51:37 +0900

    Add softap vconf keys
    Change-Id: I6bb059f1e440411f9ad289e56fe9023dc9dae388
    Signed-off-by: Seonah Moon <seonah1.moon@samsung.com>

---
+++ include/vconf-internal-softap-keys.h
@@ -0,0 +1,40 @@
+/*
+ * vconf-internal-keys
+ *
+ * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __VCONF_INTERNAL_SOFTAP_KEYS_H__
+#define __VCONF_INTERNAL_SOFTAP_KEYS_H__
+#define VCONFKEY_SOFTAP_CONNECTED_DEVICE  "memory/softap/connected_device"
+
+#define VCONFKEY_SOFTAP_SECURITY  "db/softap/security"
+
+enum {
+	VCONFKEY_MOBILE_AP_SECURITY_OFF  = 0x00,
+		VCONFKEY_MOBILE_AP_SECURITY_ON
+};
+
+#define VCONFKEY_SOFTAP_HIDE  "db/softap/hide"
+
+enum {
+	VCONFKEY_MOBILE_AP_HIDE_OFF  = 0x00,
+		VCONFKEY_MOBILE_AP_HIDE_ON
+};
+
+#define VCONFKEY_SOFTAP_SSID  "memory/private/softap/ssid"
+
+#endif /*   __VCONF_INTERNAL_SOFTAP_KEYS_H__    */
