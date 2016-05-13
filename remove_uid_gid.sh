0faf84b011aa16e31e810e4273afc5c06d279c1b
MyoungJune Park <mj2004.park@samsung.com>
2016-05-13 12:12:16 +0900

    remove the duplicates
    - update .sh utils
    
    Change-Id: I5c221d17e1e965f45b9e19937b41b517fc1729f0
    Signed-off-by: MyoungJune Park <mj2004.park@samsung.com>

---
+++ remove_uid_gid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+filelist=`ls ./data/*.xml`
+
+for file in $filelist
+do
+	xmlstarlet ed -d "/vconfkeylists/vconfkey/@uid" $file > $file.temp
+	mv $file.temp $file
+ 	xmlstarlet ed -d "/vconfkeylists/vconfkey/@gid" $file > $file.temp
+	mv $file.temp $file
+done
